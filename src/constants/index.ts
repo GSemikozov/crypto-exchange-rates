db7d516f8925ea72a446292d05e6eb7fc41cf2e8
hermansemykozov <semikozovgerman@gmail.com>
2021-10-14 18:24:43 +0300

    wip: redirects, add todo

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const PROXY = "https://cors-anywhere.herokuapp.com";
 // window.location.hostname === "localhost"
 //     ? "https://cors-anywhere.herokuapp.com"
-//     : "/cors-proxy";
+//     : "/cors-proxy"; // TODO: make it works via netlify redirects
 
 export const API_URL = "https://coin360.com/api/coins";
