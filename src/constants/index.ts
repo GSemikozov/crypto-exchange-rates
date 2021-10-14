8b8bf6836380d3a834933a9e79b2e7f377285fad
hermansemykozov <semikozovgerman@gmail.com>
2021-10-14 18:18:38 +0300

    wip: redirects

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
-export const PROXY =
-    window.location.hostname === "localhost"
-        ? "https://cors-anywhere.herokuapp.com"
-        : "/cors-proxy";
+export const PROXY = "https://cors-anywhere.herokuapp.com";
+// window.location.hostname === "localhost"
+//     ? "https://cors-anywhere.herokuapp.com"
+//     : "/cors-proxy";
 
 export const API_URL = "https://coin360.com/api/coins";
